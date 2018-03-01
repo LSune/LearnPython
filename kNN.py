8b53f97f70098ff8c640db67a0cd3ed984e7d285
unrealluver <1653673717@qq.com>
2018-03-01 20:34:13 +0800

    kNN算法相关

--- kNN.py
+++ kNN.py
@@ -17,8 +17,15 @@ def classify0(inX, dataSet, labels, k):
     classCount = {}
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
-        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1#词典默认值为0，每次统计+1
     sortedClassCount = sorted(classCount.iteritems(),
                               key = operator.itemgetter(1),
                               reverse = True)
+    '''sorted 语法：
+            sorted(iterable[, cmp[, key[, reverse]]])
+            参数说明：
+            iterable -- 可迭代对象。
+            cmp -- 比较的函数，这个具有两个参数，参数的值都是从可迭代对象中取出，此函数必须遵守的规则为，大于则返回1，小于则返回-1，等于则返回0。
+            key -- 主要是用来进行比较的元素，只有一个参数，具体的函数的参数就是取自于可迭代对象中，指定可迭代对象中的一个元素来进行排序。
+            reverse -- 排序规则，reverse = True 降序 ， reverse = False 升序（默认）。'''
     return sortedClassCount[0][0]
